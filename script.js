844b185c3a75a03b61ae2ff331e6723f69592de9
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-28 17:14:40 +0300

    Bounding box

--- script.js
+++ script.js
@@ -340,7 +340,7 @@ function createCar() {
         carcube.material.transparent = true;
         carcube.material.opacity = 0;
         carcube.material.visible = false;
-        carcube.needsUpdate = true;
+        carcube.material.needsUpdate = true;
         //carcube.visible = false;
         //carcube.material.visible = false;
         carcube.setDamping(0.1, 0.1);
