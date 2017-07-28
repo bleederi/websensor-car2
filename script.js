7c5d6164f3d5963148f2f2aae5aed54cb242026a
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-28 13:09:49 +0300

    New car model

--- script.js
+++ script.js
@@ -620,7 +620,7 @@ createGround() {
         this.carcube.add(this.texture);
         this.carcube.position.set(0, 0, 0);
         this.carcube.bb = new THREE.Box3().setFromObject(this.carcube); //create bounding box for collision detection                 
-        scene.add( carcube );
+        scene.add( this.carcube );
         this.carcube.setDamping(0.1, 0.1);
         var forcev2 = {x: 0, y: 0, z: -1000*speed};
         this.carcube.applyCentralImpulse(forcev2);
