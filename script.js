3658ce2d9ff39f28edb5032d0dbb7ec5a7edda3c
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-31 11:36:48 +0300

    Speed according to car direction

--- script.js
+++ script.js
@@ -214,12 +214,12 @@ function move(camera, car, model) //Moves the car(camera) and syncs the model to
                 if(direction == "left")
                 {
                         velocity = ({x: car.getLinearVelocity().x-2*force, y: car.getLinearVelocity().y, z: car.getLinearVelocity().z-speed*Math.cos(car.rotation.z)});
-                        forcev = {x: -forcefactor*mass*force, y: 0, z: -4*mass*speed};
+                        forcev = {x: -forcefactor*mass*force, y: 0, z: -10*mass};
                 }
                 else if (direction == "right")
                 {
                         velocity = ({x: car.getLinearVelocity().x+2*force, y: car.getLinearVelocity().y, z: car.getLinearVelocity().z-speed*Math.cos(car.rotation.z)});
-                        forcev = {x: forcefactor*mass*force, y: 0, z: -4*mass*speed};
+                        forcev = {x: forcefactor*mass*force, y: 0, z: -10*mass};
                 }
                 else    //no sensors
                 {
